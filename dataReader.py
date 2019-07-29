f32c3eb9937c0196905d264156f55cf3be2788ae
Alio Agudo <alio.agudo@soprasteria.com>
2019-07-29 10:00:17 +0200

    implementado

--- dataReader.py
+++ dataReader.py
@@ -21,19 +21,23 @@ grupos = [grupoA, grupoB, grupoC, grupoD]
 datos = datos[datos.find('user-list'):]
 clasificacionFinal = []
 
+def caparDatosParaJornada():
+    global datos
+    corte = datos.find('<div class="panel panel-gameweek standings-private" style="display: block;">')
+    datos = datos[corte:]
 
 def encuentraDatos(datos):
     name = ''
     points = ''
-    money = ''
+    jugadores = ''
     for jugador in todos:
         posAName = datos.find('data-title="')+12
         posBName = datos.find('><div class="position')-1
         name = datos[posAName:posBName]
 
-        posAMoney = datos.find('<div class="played">')+20
-        posBMoney = datos.find('</div></div><div class="points">')-1
-        money = datos[posAMoney:posBMoney]
+        posAJugadores = datos.find('<div class="played">')+20
+        posBJugadores = datos.find('</div></div><div class="points">')-9
+        jugadores = datos[posAJugadores:posBJugadores]
 
         posAPoints = datos.find('</div></div><div class="points">')+32
         posBPoints = datos.find('<span>pts</span></div></a>')-1
@@ -41,7 +45,7 @@ def encuentraDatos(datos):
 
         datos = datos[posBPoints+28:]
         resultados.append(
-            {'nombre': u''+name, 'puntos': points, 'valorEquipo': money})
+            {'nombre': u''+name, 'puntos': points, 'jugadores': jugadores})
 
 
 def compruebaIntegrantes(integrantes):
@@ -57,28 +61,23 @@ def compruebaIntegrantes(integrantes):
 def generaResultados():
     for grupo in grupos:
         puntosTotales = 0
-        valorEquipoTotal = 0
+        jugadoresTotales = 0
         NombreConjunto = ""
         for jugador in grupo:
             for coincidencia in resultados:
                 if(coincidencia.get('nombre') == jugador):
                     puntosTotales += int(coincidencia.get('puntos'))
-                    if((coincidencia.get('valorEquipo')[5:6]) == '.'):
-                        valorEquipoTotal += float(coincidencia.get('valorEquipo')[0:5])
-                    else:
-                        valorEquipoTotal += float(coincidencia.get('valorEquipo')[0:6])
+                    jugadoresTotales += int(coincidencia.get('jugadores'))
                     NombreConjunto += " | "+coincidencia.get('nombre')
-        # print(NombreConjunto+"\n Puntos:"+str(puntosTotales)+"\n Valor de Equipo:"+str(valorEquipoTotal))
+        # print(NombreConjunto+"\n Puntos:"+str(puntosTotales)+"\n Valor de Equipo:"+str(jugadoresTotal))
         # print("----------------------------------------")
 
         clasificacionFinal.append({'nombre': NombreConjunto+" |", 'puntos': str(
-            puntosTotales), 'valorEquipo': str(int(valorEquipoTotal))})
+            puntosTotales), 'jugadores': str(jugadoresTotales)+"/33 jugadores"})
 
 
 def ordenaResultados():
     global clasificacionFinal
-    clasificacionFinal=sorted(
-        clasificacionFinal, key=lambda elem: elem['valorEquipo'], reverse=True)
     clasificacionFinal=sorted(
         clasificacionFinal, key=lambda elem: elem['puntos'], reverse=True)
 
@@ -90,7 +89,7 @@ def imprimeResultados():
     for elem in clasificacionFinal:
         print(str(i)+'º ### '+elem['nombre']+' ####')
         print('Puntos: '+elem['puntos']+' pts')
-        print('Valor de Equipo:  '+elem['valorEquipo']+'M')
+        print('Valor de Equipo:  '+elem['jugadores'])
         print('----------------------------------------')
         i += 1
 
@@ -104,8 +103,8 @@ def escribeResultados():
 	i=1
 	for elem in clasificacionFinal:
 		contenido1='<li><div class="user-row"><div class="btn btn-sw-link user" href="users/5092913/alio" data-title="Alio"><div class="position">'+str(i)+'º</div><div class="pic" style="background-color: #E57373"><span class="nombre-grupo"></span></div><div class="name"><h2 class="size">' + \
-            elem['nombre']+'</h2><div class="played">'+elem['valorEquipo'] + \
-            'M</div></div><div class="points">' + \
+            elem['nombre']+'</h2><div class="played">'+elem['jugadores'] + \
+            '</div></div><div class="points">' + \
             elem['puntos']+'<span>pts</span></div></div></div></li>'
 		i += 1
 		contenido += contenido1
@@ -113,7 +112,7 @@ def escribeResultados():
 	f.write(completo)
 	f.close()
 
-
+caparDatosParaJornada()
 encuentraDatos(datos)
 if(compruebaIntegrantes(todos)):
     compruebaIntegrantes(todos)
